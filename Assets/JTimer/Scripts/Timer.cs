da9a9d34e0e9e3f0a1f2c31c1b2d031cf2d19050
LK_Ren <52132718+LK-Coco@users.noreply.github.com>
2021-07-26 12:06:07 +0800

    no message

--- Assets/JTimer/Scripts/Timer.cs
+++ Assets/JTimer/Scripts/Timer.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
 
 namespace JTimer
 {
@@ -30,17 +30,23 @@ namespace JTimer
         private bool _useScaleTime;
         private bool _isLoop;
         private int _loopTimes;
-        private float _cachedTime;
-        private float _duration;
-        private Action<float> _onUpdate;
+        private double _cachedTime;
+        private double _duration;
+        private Action<double> _onUpdate;
+        private Action<int> _onLoop;
         private Action _onComplete;
+
+        private Stopwatch _stopwatch;
+        private double _preTime;
+        private double _nowTime;
         private TimerState _state;
+        private int _currentLoop;
 
         #endregion
 
         #region Constructor
 
-        public Timer(float duration,bool useScaleTime,bool isLoop, int loopTimes,Action<float> onUpdate, Action onComplete)
+        public Timer(long duration,bool useScaleTime,bool isLoop, int loopTimes,Action<double> onUpdate, Action onComplete)
         {
             _duration = duration;
             _useScaleTime = useScaleTime;
@@ -49,23 +55,45 @@ namespace JTimer
             _onUpdate = onUpdate;
             _onComplete = onComplete;
             _state = TimerState.Inited;
+
+            _stopwatch = new Stopwatch();
         }
 
-        public Timer(float duration, bool useScaleTime, bool isLoop, int loopTimes)
+        public Timer(long duration, bool useScaleTime, bool isLoop, int loopTimes,Action onComplete)
         {
             _duration = duration;
             _useScaleTime = useScaleTime;
             _isLoop = isLoop;
             _loopTimes = loopTimes;
+            _onComplete = onComplete;
+            _state = TimerState.Inited;
+
+            _stopwatch = new Stopwatch();
+        }
+
+        public Timer(long duration, bool useScaleTime, bool isLoop, int loopTimes)
+        {
+            _duration = duration;
+            _useScaleTime = useScaleTime;
+            _isLoop = isLoop;
+            _loopTimes = loopTimes;
+
+            _stopwatch = new Stopwatch();
         }
 
         #endregion
 
         #region Public Methods
 
-        public Timer OnUpdated(Action<float> action)
+        public Timer OnUpdated(Action<double> action)
         {
             this._onUpdate = action;
+            return this;
+        }
+
+        public Timer OnLooped(Action<int> action)
+        {
+            this._onLoop = action;
             return this;
         }
 
@@ -80,6 +108,10 @@ namespace JTimer
             if (_state != TimerState.Inited) return;
             TimerManager.Instance.Register(this);
             _state = TimerState.Started;
+            _currentLoop = 0;
+
+            _stopwatch.Start();
+            _preTime = _stopwatch.Elapsed.TotalSeconds;
         }
 
         public void Pause()
@@ -90,6 +122,7 @@ namespace JTimer
         public void Resume()
         {
             _state = TimerState.Started;
+            _preTime = _stopwatch.Elapsed.TotalSeconds;
         }
 
         public void Stop()
@@ -97,22 +130,19 @@ namespace JTimer
             _state = TimerState.Stoped;
         }
 
-        #endregion
-
-        #region Internal Methods
-
-        internal void Update()
+        public void Update()
         {
             if (_state != TimerState.Started) return;
-            _cachedTime += _useScaleTime ? UnityEngine.Time.deltaTime : UnityEngine.Time.unscaledDeltaTime;
+            _cachedTime += _useScaleTime ? GetDeltaTime() * UnityEngine.Time.timeScale : GetDeltaTime();
             _onUpdate?.Invoke(_cachedTime);
             if (_cachedTime > _duration)
             {
                 _onComplete?.Invoke();
-                if (_isLoop && _loopTimes > 0)
+                if (_isLoop && _loopTimes > _currentLoop)
                 {
-                    _loopTimes--;
+                    _currentLoop++;
                     _cachedTime -= _duration;
+                    _onLoop?.Invoke(_currentLoop);
                 }
                 else
                 {
@@ -123,14 +153,27 @@ namespace JTimer
 
         #endregion
 
+        #region private Methods
+
+        private double GetDeltaTime()
+        {
+            _nowTime = _stopwatch.Elapsed.TotalSeconds;
+            var delta = _nowTime - _preTime;
+            _preTime = _nowTime;
+
+            return delta;
+        } 
+
+        #endregion
+
         #region Static Methods
 
-        public static Timer Delay(float duration, bool useScaleTime, Action onComplete)
+        public static Timer Delay(long duration, bool useScaleTime, Action onComplete)
         {
             return new Timer(duration, useScaleTime, false, 0,null,onComplete);
         }
 
-        public static Timer Repeat(float duration, bool useScaleTime, int loopTimes,Action onComplete)
+        public static Timer Repeat(long duration, bool useScaleTime, int loopTimes,Action onComplete)
         {
             return new Timer(duration, useScaleTime, true, loopTimes, null, onComplete);
         }
--- Assets/Scenes/TimerTest.cs
+++ Assets/Scenes/TimerTest.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class TimerTest : MonoBehaviour
 {
+    private int num = 0;
     // Start is called before the first frame update
     void Start()
     {
-        //var timer = new JTimer.Timer(2f, CallBack);
+        var timer = new JTimer.Timer(2,true,false,0,null, CallBack);
         //var timer2 = new JTimer.Timer(4f, CallBack);
-        //timer.Start();
+        timer.Start();
         //timer2.Start();
     }
 
@@ -28,7 +29,14 @@ public class TimerTest : MonoBehaviour
         {
             Time.timeScale = 2;
         }
-        Debug.Log(Time.realtimeSinceStartup + "  " + Time.time );
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //JTimer.Timer.Delay(2f, true, () => Debug.Log($"Invoke{num}")).Start();
+            JTimer.Timer.Repeat(2, true, 3, () => Debug.Log($"Invoke{num}")).OnUpdated(t => Debug.Log($"upddate{t}")).Start(); 
+            num++;
+        }
+        //Debug.Log(Time.realtimeSinceStartup + "  " + Time.time );
     }
 
     void CallBack()
